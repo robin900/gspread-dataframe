070909882124700897d95ec856530b4bcdc0884f
Robin Thomas <robin.thomas@livestream.com>
2017-03-28 09:50:44 -0400

    added basics of unit test suite, with accurate mock worksheet cell feed.

---
+++ tests/mock_worksheet.py
@@ -0,0 +1,19 @@
+from gspread.models import Cell
+from gspread.ns import _ns
+
+import os.path
+from xml.etree import ElementTree as ET
+
+FEED = None
+
+with open(os.path.join(os.path.dirname(__file__), 'cell_feed.xml'), 'r') as f:
+    FEED = ET.fromstring(f.read())
+
+class MockWorksheet(object):
+    def _fetch_cells(self):
+        return [Cell(self, elem) for elem in FEED.findall(_ns('entry'))]
+
+if __name__ == '__main__':
+    from gspread_dataframe import *
+    ws = MockWorksheet()
+    df = get_as_dataframe(ws)
---
+++ tests/test_read.py
@@ -0,0 +1,5 @@
+
+from gspread_dataframe import *
+
+ws = MockWorksheet()
+df = get_as_dataframe(ws)
