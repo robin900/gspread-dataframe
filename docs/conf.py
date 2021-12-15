a2543686acb4487cb5f8fbb9078037f6e4da063e
Robin Thomas <rthomas900@gmail.com>
2021-12-15 12:53:06 -0500

    push proper dir onto sys.path

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 
 # -- General configuration ------------------------------------------------
