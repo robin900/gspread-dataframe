9b0b57935c53f948364c4ac6ac1c2ade3f0cc7e6
Rafael Cruz <rafael.cruz.external@zalando.de>
2024-01-30 13:56:57 +0100

    Updates test interfaces to make use of the fixed version for get_as_dataframe.

--- tests/gspread_dataframe_test.py
+++ tests/gspread_dataframe_test.py
@@ -106,10 +106,11 @@ class TestStringEscaping(unittest.TestCase):
 
 class TestWorksheetReads(unittest.TestCase):
     def setUp(self):
-        self.sheet = MockWorksheet()
+        self.worksheet = MockWorksheet()
+        self.spreadsheet = self.worksheet.exceptionally_access_underlying_spreadsheet()
 
     def test_noargs(self):
-        df = get_as_dataframe(self.sheet)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet)
         self.assertEqual(list(df.columns.values), COLUMN_NAMES)
         self.assertEqual(len(df.columns), 10)
         self.assertEqual(len(df), 9)
@@ -118,48 +119,48 @@ class TestWorksheetReads(unittest.TestCase):
         self.assertEqual(list(df.index.values), list(range(9)))
 
     def test_evaluate_formulas_true(self):
-        df = get_as_dataframe(self.sheet, evaluate_formulas=True)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, evaluate_formulas=True)
         self.assertEqual(list(df.columns.values), COLUMN_NAMES)
         self.assertEqual(df["Formula Column"][0], 2.226)
 
     def test_evaluate_formulas_false(self):
-        df = get_as_dataframe(self.sheet)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet)
         self.assertEqual(list(df.columns.values), COLUMN_NAMES)
         self.assertEqual(df["Formula Column"][0], "=R[0]C[-1]*2")
 
     def test_usecols(self):
-        df = get_as_dataframe(self.sheet, usecols=USECOLS_COLUMN_NAMES)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, usecols=USECOLS_COLUMN_NAMES)
         self.assertEqual(list(df.columns.values), USECOLS_COLUMN_NAMES)
 
     def test_indexcol(self):
-        df = get_as_dataframe(self.sheet, index_col=4)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, index_col=4)
         self.assertEqual(len(df.columns), 9)
         self.assertEqual(df.index.name, "Date Column")
         self.assertEqual(type(df.index).__name__, "Index")
         self.assertEqual(df.index.values[0], "2017-03-04")
 
     def test_indexcol_none(self):
-        df = get_as_dataframe(self.sheet, index_col=False)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, index_col=False)
         self.assertEqual(len(df.columns), 10)
         self.assertEqual(df.index.name, None)
         self.assertEqual(type(df.index).__name__, "RangeIndex")
         self.assertEqual(list(df.index.values), list(range(9)))
 
     def test_header_false(self):
-        df = get_as_dataframe(self.sheet, header=None)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, header=None)
         self.assertEqual(len(df), 10)
 
     def test_header_first_row(self):
-        df = get_as_dataframe(self.sheet, header=0)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, header=0)
         self.assertEqual(len(df), 9)
 
     def test_skiprows(self):
-        df = get_as_dataframe(self.sheet, skiprows=range(1, 4))
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, skiprows=range(1, 4))
         self.assertEqual(len(df), 6)
 
     def test_prefix(self):
         df = get_as_dataframe(
-            self.sheet, skiprows=[0], header=None, prefix="COL"
+            self.spreadsheet, self.worksheet, skiprows=[0], header=None, prefix="COL"
         )
         self.assertEqual(len(df), 9)
         self.assertEqual(
@@ -167,13 +168,14 @@ class TestWorksheetReads(unittest.TestCase):
         )
 
     def test_squeeze(self):
-        df = get_as_dataframe(self.sheet, usecols=[0], squeeze=True)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, usecols=[0], squeeze=True)
         self.assertTrue(isinstance(df, pd.Series))
         self.assertEqual(len(df), 9)
 
     def test_converters_datetime(self):
         df = get_as_dataframe(
-            self.sheet,
+            self.spreadsheet,
+            self.worksheet,
             converters={
                 "Date Column": lambda x: datetime.strptime(x, "%Y-%m-%d")
             },
@@ -189,20 +191,21 @@ class TestWorksheetReads(unittest.TestCase):
         )
 
     def test_no_nafilter(self):
-        df = get_as_dataframe(self.sheet, na_filter=False)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, na_filter=False)
         self.assertEqual(df["Dialect-specific implementations"][7], "")
 
     def test_nafilter(self):
-        df = get_as_dataframe(self.sheet, na_filter=True)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, na_filter=True)
         self.assertTrue(np.isnan(df["Dialect-specific implementations"][7]))
 
     def test_parse_dates_true(self):
-        df = get_as_dataframe(self.sheet, index_col=4, parse_dates=True)
+        df = get_as_dataframe(self.spreadsheet, self.worksheet, index_col=4, parse_dates=True)
         self.assertEqual(df.index[0], pd.Timestamp("2017-03-04 00:00:00"))
 
     def test_parse_dates_true_infer(self):
         df = get_as_dataframe(
-            self.sheet,
+            self.spreadsheet,
+            self.worksheet,
             index_col=4,
             parse_dates=True,
             infer_datetime_format=True,
@@ -211,7 +214,8 @@ class TestWorksheetReads(unittest.TestCase):
 
     def test_parse_dates_custom_parser(self):
         df = get_as_dataframe(
-            self.sheet,
+            self.spreadsheet,
+            self.worksheet,
             parse_dates=[4],
             date_parser=lambda x: datetime.strptime(x, "%Y-%m-%d"),
         )
